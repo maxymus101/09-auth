e945b50a68d2c0fd68b2fbaa562f5d7b26bae79d
Max Akimov <maxymus2828@gmail.com>
2025-07-19 22:42:03 +0300

    fix 1.0

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,14 +1,7 @@
 import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
-
-const TOKEN = process.env.NEXT_PUBLIC_API_URL;
-
 export const nextServer = axios.create({
   baseURL,
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${TOKEN}`,
-  },
 });
